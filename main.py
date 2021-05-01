22ae0a222300e96fb5814600f0c8a4e94c008cd0
Pranav Patki <66993183+pranav0609@users.noreply.github.com>
2021-05-02 00:28:09 +0530

    main and registration pages added

---
+++ main.py
@@ -0,0 +1,134 @@
+from tkinter import *
+import tkinter.messagebox
+from tkinter import ttk
+import random
+import time
+import datetime
+
+
+def main():
+    root = Tk()
+    app = Window1(root)
+    root.mainloop()
+
+
+class Window1:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Pharmacy Management SoftWare')
+        self.master.geometry('1350x750+0+0')
+        self.frame = Frame(self.master)
+        self.frame.pack()
+
+        self.Username = StringVar()
+        self.Password = StringVar()
+
+        self.LabelTitle = Label(self.frame, text='Pharmacy Management System', font=('arial', 50, 'bold'), bd=20)
+        self.LabelTitle.grid(row=0, column=0, columnspan=2, pady=20)
+
+        self.LoginFrame1 = Frame(self.frame, width=1000, height=300, bd=20, relief='ridge')
+        self.LoginFrame1.grid(row=1, column=0)
+
+        self.LoginFrame2 = Frame(self.frame, width=1000, height=100, bd=20, relief='ridge')
+        self.LoginFrame2.grid(row=2, column=0)
+
+        self.LoginFrame3 = Frame(self.frame, width=1000, height=200, bd=20, relief='ridge')
+        self.LoginFrame3.grid(row=3, column=0, pady=2)
+
+        # ==================================================================================================
+
+        self.LabelUsername = Label(self.LoginFrame1, text='Username', font=('arial', 30, 'bold'), bd=20)
+        self.LabelUsername.grid(row=0, column=0)
+        self.TextUsername = Entry(self.LoginFrame1, font=('arial', 30, 'bold'), bd=20, textvariable=self.Username)
+        self.TextUsername.grid(row=0, column=1)
+
+        self.LabelPassword = Label(self.LoginFrame1, text='Password', font=('arial', 30, 'bold'), bd=20)
+        self.LabelPassword.grid(row=1, column=0)
+        self.TextPassword = Entry(self.LoginFrame1, font=('arial', 30, 'bold'), bd=20, textvariable=self.Password)
+        self.TextPassword.grid(row=1, column=1)
+        # ==================================================================================================
+
+        self.Login = Button(self.LoginFrame2, text='Login', width=17, font=('arial', 20, 'bold'),
+                            command=self.Logging_in)
+        self.Login.grid(row=0, column=0)
+
+        self.Reset = Button(self.LoginFrame2, text='Reset', width=17, font=('arial', 20, 'bold'),
+                            command=self.Resetting)
+        self.Reset.grid(row=0, column=1)
+
+        self.Exit = Button(self.LoginFrame2, text='Exit', width=17, font=('arial', 20, 'bold'),
+                           command=self.Exitting)
+        self.Exit.grid(row=0, column=2)
+
+        # ==================================================================================================
+
+        self.patient_button = Button(self.LoginFrame3, text='Patient Registration', font=('arial', 20, 'bold'),
+                                     state=DISABLED, command=self.Patient_window)
+        self.patient_button.grid(row=0, column=0)
+
+        self.hospital_button = Button(self.LoginFrame3, text='Hospital Registration', font=('arial', 20, 'bold'),
+                                      state=DISABLED, command=self.Hospital_window)
+        self.hospital_button.grid(row=0, column=1)
+
+        # ==================================================================================================
+
+    def Logging_in(self):
+        user = (self.Username.get())
+        pass_word = (self.Password.get())
+
+        if user == str(12345) and pass_word == str(12345):
+            self.patient_button.config(state=NORMAL)
+            self.hospital_button.config(state=NORMAL)
+        else:
+            tkinter.messagebox.askyesno('Pharmacy Management System',
+                                        'Invalid Credentials! Do you want to retry?')
+            self.patient_button.config(state=DISABLED)
+            self.hospital_button.config(state=DISABLED)
+            self.Username.set('')
+            self.Password.set('')
+            self.TextUsername.focus()
+
+    def Resetting(self):
+        self.patient_button.config(state=DISABLED)
+        self.hospital_button.config(state=DISABLED)
+        self.Username.set('')
+        self.Password.set('')
+        self.TextUsername.focus()
+
+    def Exitting(self):
+        self.Exitting = tkinter.messagebox.askyesno('Pharmacy Management System', 'Do you want to exit?')
+        if self.Exitting > 0:
+            self.master.destroy()
+            return
+
+        # ==================================================================================================
+
+    def Patient_window(self):
+        self.new_window = Toplevel(self.master)
+        self.app = Window2(self.new_window)
+
+    def Hospital_window(self):
+        self.new_window = Toplevel(self.master)
+        self.app = Window3(self.new_window)
+
+
+class Window2:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Patient Registration')
+        self.master.geometry('1350x750+0+0')
+        self.frame = Frame(self.master)
+        self.frame.pack()
+
+
+class Window3:
+    def __init__(self, master):
+        self.master = master
+        self.master.title('Hospital Management')
+        self.master.geometry('1350x750+0+0')
+        self.frame = Frame(self.master)
+        self.frame.pack()
+
+
+if __name__ == '__main__':
+    main()
---
+++ registration.py
@@ -0,0 +1,193 @@
+from tkinter import *
+from tkinter import ttk
+import random
+import time;
+import datetime
+import tkinter.messagebox
+
+class Registration:
+    def __init__(self, root):
+        self.root = root
+        self.root.title("User registration window")
+        self.root.geometry("1350x750+0+0")
+        self.root.configure(background="black")
+
+        DateOfOrder = StringVar()
+        DateOfOrder.set(time.strftime("%d/%m/%Y"))
+        var1=StringVar()
+        var2=StringVar()
+        var3=StringVar()
+        var4=IntVar()
+
+        Firstname = StringVar()
+        Surname = StringVar()
+        Address = StringVar()
+        PsotalCode = StringVar()
+        telephone = StringVar()
+        Ref = StringVar()
+
+        membership = StringVar()
+        membership.set("0")
+
+        #===============Functions==================
+        def iExit():
+            iExit = tkinter.messagebox.askyesno("registration window", "are you sure to exit?")
+            if iExit>0:
+                root.destroy()
+                return
+
+        def Reset():
+            Firstname.set("")
+            Surname.set("")
+            Address.set("")
+            PsotalCode.set("")
+            telephone.set("")
+            Ref.set("")
+            membership.set("0")
+
+            var1.set("")
+            var2.set("")
+            var3.set("")
+            var4.set(0)
+
+            self.cboProofOfID.current(0)
+            self.cboTypeOfMember.current(0)
+            self.cboMethodofPayment.current(0)
+        def iResetRecord():
+            iResetRecord = tkinter.messagebox.askyesno("registration window", "do you want to reset all registered users to 0 too?")
+            if iResetRecord<=0:
+                Reset()
+            elif iResetRecord>0:
+                Reset()
+                self.txtReceipt.delete("1.0", END)
+                return
+        def Ref_no_Generator():
+            x=random.randint(10903, 68921)
+            random_Ref = str(x)
+            Ref.set(random_Ref)
+        
+        def Record_Add():
+            Ref_no_Generator()
+            self.txtReceipt.insert(END, "\t\t" + Ref.get()+ "\t\t" + Firstname.get() + "\t\t" + Surname.get() +
+            "\t\t"+Address.get()+"\t\t"+DateOfOrder.get() + "\t\t"+ telephone.get()+"\n")
+
+
+    #=========================Frames=========================
+        MainFrame = Frame(self.root)
+        MainFrame.grid()
+
+        Ttileframe = Frame(MainFrame, bd=20, width=1350, padx=26, relief=RIDGE)
+        Ttileframe.pack(side=TOP)
+
+        self.lblTtile = Label(Ttileframe, font=('arial', 50, 'bold'), text="User Registration Window", padx = 2)
+        self.lblTtile.grid()
+
+    #===================Lower frames ====================
+        MemberDeatilsFrame = LabelFrame(MainFrame, bd = 20, width=1350, height = 500,  pady = 5, relief = RIDGE)
+        MemberDeatilsFrame.pack(side=BOTTOM)
+
+        FrameDetails = LabelFrame(MemberDeatilsFrame, width=880, bd=10, height=400, relief=RIDGE)
+        FrameDetails.pack(side=LEFT)
+
+        MembersName_F = LabelFrame(FrameDetails, bd=10, width=350, height=450, font=('arial', 20, 'bold'), text="Customer Name ", relief=RIDGE)
+        MembersName_F.grid(row=0, column=0)
+
+        ReceiptButtomFrame = LabelFrame(MemberDeatilsFrame, bd=10, width=1000, height=450, relief=RIDGE)
+        ReceiptButtomFrame.pack(side=RIGHT)
+
+        #===============================================
+        self.lblReferenceNo = Label(MembersName_F, font=('arial', 14, 'bold'), text="Reference no", bd = 7)
+        self.lblReferenceNo.grid(row=0, column=0)
+        self.txtReferenceNo = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Ref,
+         state=DISABLED, insertwidth=2)
+        self.txtReferenceNo.grid(row=0, column=1)
+
+        self.lblFirstName = Label(MembersName_F, font=('arial', 14, 'bold'), text="Name", bd = 7)
+        self.lblFirstName.grid(row=1, column=0)
+        self.txtFirstName = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Firstname,
+         insertwidth=2)
+        self.txtFirstName.grid(row=1, column=1)
+
+        self.lblLastName = Label(MembersName_F, font=('arial', 14, 'bold'), text="Username", bd = 7)
+        self.lblLastName.grid(row=2, column=0)
+        self.txtLastName = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Surname,
+         insertwidth=2)
+        self.txtLastName.grid(row=2, column=1)
+        
+        self.lblPostalCode = Label(MembersName_F, font=('arial', 14, 'bold'), text="Password", bd = 7)
+        self.lblPostalCode.grid(row=3, column=0)
+        self.txtPostalCode = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=PsotalCode,
+         insertwidth=2)
+        self.txtPostalCode.grid(row=3, column=1)
+        
+        self.lblAddress = Label(MembersName_F, font=('arial', 14, 'bold'), text="Address", bd = 7)
+        self.lblAddress.grid(row=4, column=0)
+        self.txtAddress = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Address,
+         insertwidth=2)
+        self.txtAddress.grid(row=4, column=1)
+        
+        self.lblTele = Label(MembersName_F, font=('arial', 14, 'bold'), text="Telephone No", bd = 7)
+        self.lblTele.grid(row=5, column=0)
+        self.txtTele = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=telephone,
+         insertwidth=2)
+        self.txtTele.grid(row=5, column=1)
+        
+        self.lblDate = Label(MembersName_F, font=('arial', 14, 'bold'), text="Date", bd = 7)
+        self.lblDate.grid(row=6, column=0)
+        self.txtDate = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=DateOfOrder,
+         insertwidth=2)
+        self.txtDate.grid(row=6, column=1)
+        
+        #===============================================
+
+        #===================Member info================
+        self.lblProveOfID = Label(MembersName_F, font=('arial', 14, 'bold'), text="Proof of identity", bd = 7)
+        self.lblProveOfID.grid(row=7, column=0)
+
+        self.cboProofOfID = ttk.Combobox(MembersName_F, textvariable=var1, state='readonly', font=('arial', 14, 'bold'), width=19)
+        self.cboProofOfID['value'] = ('', 'Drivers license', 'electricity bill', 'adhar card', 'passport')
+        self.cboProofOfID.current(0)
+        self.cboProofOfID.grid(row=7, column=1)
+
+        self.lblTypeOfMember = Label(MembersName_F, font=('arial', 14, 'bold'), text="Type of membership", bd = 7)
+        self.lblTypeOfMember.grid(row=8, column=0)
+
+        self.cboTypeOfMember = ttk.Combobox(MembersName_F, textvariable=var2, state='readonly', font=('arial', 14, 'bold'), width=19)
+        self.cboTypeOfMember['value'] = ('', 'Monthly member', 'annual member', 'weekly member', 'other')
+        self.cboTypeOfMember.current(0)
+        self.cboTypeOfMember.grid(row=8, column=1)
+
+        self.lblMethodOfpayment = Label(MembersName_F, font=('arial', 14, 'bold'), text="Payment method", bd = 7)
+        self.lblMethodOfpayment.grid(row=9, column=0)
+
+        self.cboMethodofPayment = ttk.Combobox(MembersName_F, textvariable=var3, state='readonly', font=('arial', 14, 'bold'), width=19)
+        self.cboMethodofPayment['value'] = ('', 'Cash', 'Credit card', 'Debit  card', 'UPI wallets')
+        self.cboMethodofPayment.current(0)
+        self.cboMethodofPayment.grid(row=9, column=1)
+
+        #==============Receipt=====================
+        self.lblLabel = Label(ReceiptButtomFrame, font=('arial', 10, 'bold'), pady=10, 
+        text='Member reference\t First Name \t username \t Address \t Date of registration \t Telephone', bd=7
+        )
+        self.lblLabel.grid(row=0, column=0, columnspan=4)
+
+        self.txtReceipt = Text(ReceiptButtomFrame, width=112, height=22, font=('arial', 10, 'bold'))
+        self.txtReceipt.grid(row=1, column=0, columnspan=4)
+
+        #===================Buttons=================
+        self.btnReceipt = Button(ReceiptButtomFrame, padx=18, bd=7, font=('arial', 16, 'bold'), width=13,
+        text='Add', command=Record_Add).grid(row=2, column=0)
+        self.btnReset = Button(ReceiptButtomFrame, padx=18, bd=7, font=('arial', 16, 'bold'), width=13,
+        text='Reset', command= iResetRecord).grid(row=2, column=1)
+        self.btnExit = Button(ReceiptButtomFrame, padx=18, bd=7, font=('arial', 16, 'bold'), width=13,
+        text='Exit', command=iExit).grid(row=2, column=2)
+
+
+
+
+
+if __name__ == "__main__":
+    root = Tk()
+    application = Registration(root)
+    root.mainloop()
+    
