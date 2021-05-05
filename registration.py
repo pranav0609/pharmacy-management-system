392b1940b7b8f328b3f8425008bd959c2a210967
Pranav Patki <66993183+pranav0609@users.noreply.github.com>
2021-05-06 00:05:18 +0530

    database changes and amount added

--- registration.py
+++ registration.py
@@ -9,7 +9,7 @@ import tkinter.messagebox
 class Registration:
     def __init__(self, root):
         self.root = root
-        self.root.title("User registration window")
+        self.root.title("Patient Receipt window")
         self.root.geometry("1350x750+0+0")
         self.root.configure(background="black")
 
@@ -18,17 +18,18 @@ class Registration:
         var1=StringVar()
         var2=StringVar()
         var3=StringVar()
+        amount = StringVar()
         var4=IntVar()
 
         Firstname = StringVar()
         Surname = StringVar()
         Address = StringVar()
-        DOB = StringVar()
+        med_quantity = StringVar()
         telephone = StringVar()
         Ref = StringVar()
 
         adhar = StringVar()
-        adhar.set("0")
+        adhar.set("")
 
         #===============Functions==================
         def iExit():
@@ -39,9 +40,9 @@ class Registration:
 
         def Reset():
             Firstname.set("")
-            Surname.set("")
+            #Surname.set("")
             Address.set("")
-            DOB.set("")
+            med_quantity.set("")
             telephone.set("")
             Ref.set("")
             adhar.set("0")
@@ -51,15 +52,16 @@ class Registration:
             var3.set("")
             var4.set(0)
 
-            self.cboProofOfID.current(0)
-            self.cboTypeOfMember.current(0)
+            self.cboMedicince.current(0)
+            #self.cboTypeOfMember.current(0)
             self.cboMethodofPayment.current(0)
         def iResetRecord():
-            iResetRecord = tkinter.messagebox.askyesno("registration window", "do you want to reset all registered users to 0 too?")
+            iResetRecord = tkinter.messagebox.askokcancel("registration window", "do you want to reset all registered users to 0 too?")
             if iResetRecord<=0:
                 Reset()
             elif iResetRecord>0:
                 Reset()
+                #self.txtReceipt.config(state=NORMAL)
                 self.txtReceipt.delete("1.0", END)
                 return
         def Ref_no_Generator():
@@ -68,32 +70,52 @@ class Registration:
             Ref.set(random_Ref)
         
         def Record_Add():
-            Ref_no_Generator()
+            #Ref_no_Generator()
             patient_id = Ref.get()
             patient_name = Firstname.get()
             patient_address = Address.get()
             patient_tele = telephone.get()
-            patient_dob = DOB.get()
+            quantity = med_quantity.get()
+            medicine = var1.get()
             adhaar_number = adhar.get()
             patient_room = 3
-            if(patient_id=="" or patient_name=="" or patient_address=="" or patient_tele==""or patient_dob==""or patient_room==0 or adhaar_number==""):
+            if(patient_id=="" or patient_name=="" or patient_address=="" or patient_tele==""or quantity==""or patient_room==0 or adhaar_number==""):
                 tkinter.messagebox.showinfo("inert status", "all fields are not filled")
             else:
                 
-                self.txtReceipt.insert(END, "\t\t" + Ref.get()+ "\t\t" + Firstname.get() + "\t\t" + Surname.get() +
-            "\t\t"+Address.get()+"\t\t"+DateOfOrder.get() + "\t\t"+ telephone.get()+"\n")
+                self.txtReceipt.insert(END, "\t\t" + Ref.get()+ "\t\t" + Firstname.get() + "\t\t" + telephone.get()
+                +"\t\t"+DateOfOrder.get() + "\t\t"+ "\t"+ var1.get()+"\t\t"+ med_quantity.get()+"\n")
                 connection = MySQLdb.connect(host='localhost', user='root', password='12345')
                 string = "use pythondb"
                 cursor = connection.cursor()
                 cursor.execute(string)
                 
-                string = "insert into patients values('%s','%s', '%s', '%s', '%d', '%s', '%s')"
-                args = (patient_name, patient_id, patient_dob, adhaar_number, patient_room, patient_address, patient_tele)
+                string = "insert into patients values('%s','%s', '%s', '%s', '%s', '%s', '%s', '%s')"
+                args = ( patient_name, patient_id, adhaar_number, patient_room, patient_address, patient_tele, medicine, quantity)
                 cursor.execute(string % args)
                 connection.commit()
                 tkinter.messagebox.showinfo("insert status", "patient inserted successfully")
                 cursor.close()
+        def Price():
+            if(var4.get() == 1):
                 
+                quan = int(med_quantity.get())
+                self.txtAmount.configure(state=NORMAL)
+                if(var1.get() == "Paracetamol"):
+                    item1 = int(50*quan) 
+                    amount.set("$"+str(item1))
+                elif(var1.get() == "Penicillin"):
+                    item1 = int(40*quan)
+                    amount.set("$"+str(item1)) 
+                elif(var1.get() == "Cod liver oil Tabs"):
+                    item1 = int(30*quan) 
+                    amount.set("$"+str(item1)) 
+                elif(var1.get() == "Benadryl"):
+                    item1 = int(20*quan) 
+                    amount.set("$"+str(item1)) 
+            elif(var4.get() == 0):
+                self.txtAmount.configure(state=DISABLED)
+                amount.set("0")
 
 
 
@@ -104,7 +126,7 @@ class Registration:
         Ttileframe = Frame(MainFrame, bd=20, width=1350, padx=26, relief=RIDGE)
         Ttileframe.pack(side=TOP)
 
-        self.lblTtile = Label(Ttileframe, font=('arial', 50, 'bold'), text="User Registration Window", padx = 2)
+        self.lblTtile = Label(Ttileframe, font=('arial', 50, 'bold'), text="Patient Reciept Window", padx = 2)
         self.lblTtile.grid()
 
     #===================Lower frames ====================
@@ -123,8 +145,7 @@ class Registration:
         #===============================================
         self.lblReferenceNo = Label(MembersName_F, font=('arial', 14, 'bold'), text="Reference no", bd = 7)
         self.lblReferenceNo.grid(row=0, column=0)
-        self.txtReferenceNo = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Ref,
-         state=DISABLED, insertwidth=2)
+        self.txtReferenceNo = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Ref, insertwidth=2)
         self.txtReferenceNo.grid(row=0, column=1)
 
         self.lblFirstName = Label(MembersName_F, font=('arial', 14, 'bold'), text="Name", bd = 7)
@@ -133,72 +154,70 @@ class Registration:
          insertwidth=2)
         self.txtFirstName.grid(row=1, column=1)
 
-        self.lblLastName = Label(MembersName_F, font=('arial', 14, 'bold'), text="Username", bd = 7)
-        self.lblLastName.grid(row=2, column=0)
-        self.txtLastName = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Surname,
-         insertwidth=2)
-        self.txtLastName.grid(row=2, column=1)
+        # self.lblLastName = Label(MembersName_F, font=('arial', 14, 'bold'), text="Username", bd = 7)
+        # self.lblLastName.grid(row=2, column=0)
+        # self.txtLastName = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Surname,
+        #  insertwidth=2)
+        # self.txtLastName.grid(row=2, column=1)
         
-        self.lblPostalCode = Label(MembersName_F, font=('arial', 14, 'bold'), text="DOB", bd = 7)
-        self.lblPostalCode.grid(row=3, column=0)
-        self.txtPostalCode = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=DOB,
+        self.lblQuantity = Label(MembersName_F, font=('arial', 14, 'bold'), text="Quantity", bd = 7)
+        self.lblQuantity.grid(row=7, column=0)
+        self.txtQuantity = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=med_quantity,
          insertwidth=2)
-        self.txtPostalCode.grid(row=3, column=1)
+        self.txtQuantity.grid(row=7, column=1)
         
         self.lblAddress = Label(MembersName_F, font=('arial', 14, 'bold'), text="Address", bd = 7)
-        self.lblAddress.grid(row=4, column=0)
+        self.lblAddress.grid(row=2, column=0)
         self.txtAddress = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=Address,
          insertwidth=2)
-        self.txtAddress.grid(row=4, column=1)
+        self.txtAddress.grid(row=2, column=1)
         
         self.lblTele = Label(MembersName_F, font=('arial', 14, 'bold'), text="Telephone No", bd = 7)
-        self.lblTele.grid(row=5, column=0)
+        self.lblTele.grid(row=3, column=0)
         self.txtTele = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=telephone,
          insertwidth=2)
-        self.txtTele.grid(row=5, column=1)
+        self.txtTele.grid(row=3, column=1)
 
         self.lblAdhar = Label(MembersName_F, font=('arial', 14, 'bold'), text="Adhar no", bd = 7)
-        self.lblAdhar.grid(row=6, column=0)
+        self.lblAdhar.grid(row=4, column=0)
         self.txtAdhar = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=adhar,
          insertwidth=2)
-        self.txtAdhar.grid(row=6, column=1)
+        self.txtAdhar.grid(row=4, column=1)
         
         self.lblDate = Label(MembersName_F, font=('arial', 14, 'bold'), text="Date", bd = 7)
-        self.lblDate.grid(row=7, column=0)
+        self.lblDate.grid(row=5, column=0)
         self.txtDate = Entry(MembersName_F, font=('arial', 14, 'bold'),  bd=7, textvariable=DateOfOrder,
          insertwidth=2)
-        self.txtDate.grid(row=7, column=1)
+        self.txtDate.grid(row=5, column=1)
         
         #===============================================
 
         #===================Member info================
-        self.lblProveOfID = Label(MembersName_F, font=('arial', 14, 'bold'), text="Proof of identity", bd = 7)
-        self.lblProveOfID.grid(row=8, column=0)
+        self.lblMedicine = Label(MembersName_F, font=('arial', 14, 'bold'), text="Medicine", bd = 7)
+        self.lblMedicine.grid(row=6, column=0)
 
-        self.cboProofOfID = ttk.Combobox(MembersName_F, textvariable=var1, state='readonly', font=('arial', 14, 'bold'), width=19)
-        self.cboProofOfID['value'] = ('', 'Drivers license', 'electricity bill', 'adhar card', 'passport')
-        self.cboProofOfID.current(0)
-        self.cboProofOfID.grid(row=8, column=1)
-
-        self.lblTypeOfMember = Label(MembersName_F, font=('arial', 14, 'bold'), text="Type of membership", bd = 7)
-        self.lblTypeOfMember.grid(row=9, column=0)
-
-        self.cboTypeOfMember = ttk.Combobox(MembersName_F, textvariable=var2, state='readonly', font=('arial', 14, 'bold'), width=19)
-        self.cboTypeOfMember['value'] = ('', 'Monthly member', 'annual member', 'weekly member', 'other')
-        self.cboTypeOfMember.current(0)
-        self.cboTypeOfMember.grid(row=9, column=1)
+        self.cboMedicince = ttk.Combobox(MembersName_F, textvariable=var1, state='readonly', font=('arial', 14, 'bold'), width=19)
+        self.cboMedicince['value'] = ('', 'Paracetamol', 'Penicillin', 'Cod liver oil Tabs', 'Benadryl')
+        self.cboMedicince.current(0)
+        self.cboMedicince.grid(row=6, column=1)
+        self.chkAmount = Checkbutton(MembersName_F, text="Refresh Amount",font=('arial', 14, 'bold'), variable=var4, onvalue=1, offvalue=0, command=Price).grid(row=8, column=0)
+        # self.lblAmount = Label(MembersName_F, font=('arial', 14, 'bold'), text="Total Amount", bd = 7)
+        # self.lblAmount.grid(row=8, column=1)
+        self.txtAmount = Entry(MembersName_F, font=('arial', 14, 'bold'), textvariable=amount,  bd=7,
+         insertwidth=2)
+        self.txtAmount.grid(row=8, column=1)
 
         self.lblMethodOfpayment = Label(MembersName_F, font=('arial', 14, 'bold'), text="Payment method", bd = 7)
-        self.lblMethodOfpayment.grid(row=10, column=0)
+        self.lblMethodOfpayment.grid(row=9, column=0)
 
         self.cboMethodofPayment = ttk.Combobox(MembersName_F, textvariable=var3, state='readonly', font=('arial', 14, 'bold'), width=19)
         self.cboMethodofPayment['value'] = ('', 'Cash', 'Credit card', 'Debit  card', 'UPI wallets')
         self.cboMethodofPayment.current(0)
-        self.cboMethodofPayment.grid(row=10, column=1)
+        self.cboMethodofPayment.grid(row=9, column=1)
 
         #==============Receipt=====================
         self.lblLabel = Label(ReceiptButtomFrame, font=('arial', 10, 'bold'), pady=10, 
-        text='Member reference\t First Name \t username \t Address \t Date of registration \t Telephone', bd=7
+        text='Patient ID\t Patient Name \t Telephone \t Date of Registration \t Medicine \t Quantity', bd=7
         )
         self.lblLabel.grid(row=0, column=0, columnspan=4)
 
